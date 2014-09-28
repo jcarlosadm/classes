60234f31a90567d5416e322751fdc707378aa071
jcarlosadm <carlosmaster10@gmail.com>
2014-09-28 12:53:52 -0300

    codigo de exemplo da questao b01 do exercicio sobre lista alterado

--- dataStructure/exercises/list/b01.c
+++ dataStructure/exercises/list/b01.c
@@ -1,50 +1,104 @@
-typedef struct node Node;
+/**
+Este arquivo possui uma função main
+*/
 
-typedef struct circularLinkedList CircularLinkedList;
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node Node;
 
 struct node{
     int item;
-    Node* nextNode;
     Node* previousNode;
+    Node* nextNode;
+    int sentinel;
 };
 
-struct circularLinkedList{
-    
-    Node* head;
-    Node* tail;
-    int size;
-    
-};
-
-CircularLinkedList* createCircularLinkedList(){
-    CircularLinkedList* newList = (CircularLinkedList*)malloc(sizeof(CircularLinkedList));
-    newList->head=NULL;
-    newList->tail=NULL;
-    newList->size=0;
+Node* createCircularList(){
+    return NULL;
 }
 
-CircularLinkedList* insertNode(CircularLinkedList* list, int item){
+Node* insertCircularNode(Node* first, int item){
     
     Node* newNode = NULL;
     newNode = (Node*)malloc(sizeof(Node));
-    if(!newNode) return list;
+    if(!newNode) return first;
     
     newNode->item = item;
+    newNode->sentinel = 1;
     
-    if(list->head==NULL){
+    if(first==NULL){
         newNode->nextNode = newNode;
         newNode->previousNode = newNode;
-        list->head = newNode;
-        list->tail = newNode;
-    }
-    else{
-        newNode->nextNode = list->head;
-        newNode->previousNode = list->tail;
-        list->head->previousNode = newNode;
-        list->tail->nextNode = newNode;
-        list->head = newNode;
+        return newNode;
     }
     
-    (list->size)++;
+    newNode->nextNode = first;
+    newNode->previousNode = first->previousNode;
+    first->previousNode = newNode;
+    newNode->previousNode->nextNode = newNode;
+    return newNode;
     
 }
+
+Node* freeCircularList(Node* current, Node *first){
+    if(current==NULL)
+        return NULL;
+    if(current==first){
+        if(current->sentinel==1){
+            current->sentinel=0;
+            current->nextNode = freeCircularList(current->nextNode,first);
+            free(current);
+            return NULL;
+        }
+        else{
+            current->sentinel = 1;
+            return current;
+        }
+    }
+    else{
+        current->nextNode = freeCircularList(current->nextNode,first);
+        free(current);
+        current = NULL;
+        return current;
+    }
+}
+
+void printCircular(Node* current, Node* first){
+    if(current==NULL)
+        return;
+    if(current==first){
+        if(current->sentinel == 1){
+            current->sentinel = 0;
+            printf(" %d",current->item);
+            printCircular(current->nextNode,first);
+        }
+        else{
+            printf("\n");
+            current->sentinel=1;
+        }
+    }
+    else{
+        printf(" %d",current->item);
+        printCircular(current->nextNode,first);
+    }
+}
+
+
+int main()
+{
+    
+    Node* node = createCircularList();
+    
+    node = insertCircularNode(node,2);
+    node = insertCircularNode(node,6);
+    node = insertCircularNode(node,1);
+    
+    printCircular(node,node);
+    
+    node = freeCircularList(node,node);
+    
+    return 0;
+}
+
+
