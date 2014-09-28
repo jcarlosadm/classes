c7cd4e89fdbea40cc8cfe47f6a650ed0e09950d8
jcarlosadm <carlosmaster10@gmail.com>
2014-09-28 12:07:53 -0300

    notas exercicios sobre lista

--- dataStructure/exercises/list/latex/code/b01.c
+++ dataStructure/exercises/list/latex/code/b01.c
@@ -1,51 +1,21 @@
-typedef struct node Node;
-
-typedef struct circularLinkedList CircularLinkedList;
-
-struct node{
-    int item;
-    Node* nextNode;
-    Node* previousNode;
-};
-
-struct circularLinkedList{
-    
-    Node* head;
-    Node* tail;
-    int size;
-    
-};
-
-CircularLinkedList* createCircularLinkedList(){
-    CircularLinkedList* newList = (CircularLinkedList*)
-                                malloc(sizeof(CircularLinkedList));
-    newList->head=NULL;
-    newList->tail=NULL;
-    newList->size=0;
-}
-
-CircularLinkedList* insertNode(CircularLinkedList* list, int item){
+Node* insertCircularNode(Node* first, int item){
     
     Node* newNode = NULL;
     newNode = (Node*)malloc(sizeof(Node));
-    if(!newNode) return list;
+    if(!newNode) return first;
     
     newNode->item = item;
     
-    if(list->head==NULL){
+    if(first==NULL){
         newNode->nextNode = newNode;
         newNode->previousNode = newNode;
-        list->head = newNode;
-        list->tail = newNode;
-    }
-    else{
-        newNode->nextNode = list->head;
-        newNode->previousNode = list->tail;
-        list->head->previousNode = newNode;
-        list->tail->nextNode = newNode;
-        list->head = newNode;
+        return newNode;
     }
     
-    (list->size)++;
+    newNode->nextNode = first;
+    newNode->previousNode = first->previousNode;
+    first->previousNode = newNode;
+    newNode->previousNode->nextNode = newNode;
+    return newNode;
     
 }
