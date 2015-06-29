12a883d60498a202c58ec31dd4d7c4d06f35764b
jcarlos <carlosmaster10@gmail.com>
2015-06-29 11:16:35 -0300

    remove patterns folder

--- p3/patterns/src/bridge/example/implement/DrawApi.java
+++
@@ -1,7 +0,0 @@
-package bridge.example.implement;
-
-public interface DrawApi {
-
-    public void drawCircle(int x, int y, int radius);
-    public void drawSquare(int x, int y, int height, int width);
-}
